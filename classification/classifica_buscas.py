fddd7659cad771627c16f7bda6da56f3ce80b5d3
Roberto Rosa <roberto.rosa7@gmail.com>
2022-04-26 09:43:41 -0300

    update

--- classification/classifica_buscas.py
+++ classification/classifica_buscas.py
@@ -13,4 +13,29 @@ y_dummies_df = y_df
 x = x_dummies_df.values
 y = y_dummies_df.values
 
-print(y)
+porcentagem_treino = 0.9
+tamanho_de_treino = int(porcentagem_treino * len(y))
+tamanho_de_teste = int(len(y) - tamanho_de_treino)
+
+treino_x = x[:tamanho_de_treino]
+treino_y = y[:tamanho_de_treino]
+
+teste_x = x[-tamanho_de_teste:]
+teste_y = y[-tamanho_de_teste:]
+
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+modelo.fit(treino_x, treino_y)
+
+resultado = modelo.predict(teste_x)
+diferencas = resultado - teste_y
+
+acertos = [d for d in diferencas if d == 0]
+total_acertos = len(acertos)
+total_elementos = len(teste_y)
+taxa_acertos = 100.0 * total_acertos / total_elementos
+
+
+print(total_elementos)
+print(taxa_acertos)
+
